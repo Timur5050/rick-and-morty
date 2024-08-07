3dcc43187ab010a2ebbad2bfc1510e0c9bf99dcd
timur <stukantimur811@gmail.com>
2024-08-07 09:51:45 +0300

    created new url for list characters view

--- characters/urls.py
+++ characters/urls.py
@@ -1,9 +1,10 @@
-from characters.views import get_random_character
+from characters.views import get_random_character, ListCharacters
 
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path("random/", get_random_character),
+    path("characters/", ListCharacters.as_view())
 ]
 
 app_name = "characters"
