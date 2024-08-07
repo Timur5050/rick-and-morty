e8afc237d28cecc1ced2088bf96a11a0a284fb83
timur <stukantimur811@gmail.com>
2024-08-07 10:04:19 +0300

    created swagger documentation using drf-spectecular

--- characters/views.py
+++ characters/views.py
@@ -1,5 +1,6 @@
 import random
 
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework.generics import ListAPIView
 
 from characters.models import Characters
@@ -12,8 +13,12 @@ from rest_framework.status import HTTP_200_OK
 from characters.serializers import CharacterSerializer
 
 
+@extend_schema(
+    responses={200: CharacterSerializer},
+)
 @api_view(["GET"])
 def get_random_character(request: Request) -> Response:
+    """get random character from characters"""
     pks = Characters.objects.values_list("pk", flat=True)
     random_pk = random.choice(pks)
     random_character = Characters.objects.get(pk=random_pk)
@@ -32,3 +37,17 @@ class ListCharacters(ListAPIView):
             queryset = queryset.filter(name__icontains=name)
 
         return queryset
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="name",
+                description="filter by name",
+                required=False,
+                type=str
+            ),
+        ]
+    )
+    def get(self, request, *args, **kwargs):
+        """List characters with filter by name"""
+        return super().get(self, request, *args, **kwargs)
