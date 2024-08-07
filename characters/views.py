9b96293b4346db052ebaa08835ff208652508370
timur <stukantimur811@gmail.com>
2024-08-07 09:51:20 +0300

    Created new view for getting filtered list of characters

--- characters/views.py
+++ characters/views.py
@@ -1,10 +1,13 @@
 import random
 
+from rest_framework.generics import ListAPIView
+
 from characters.models import Characters
 
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 
 from characters.serializers import CharacterSerializer
 
@@ -15,4 +18,17 @@ def get_random_character(request: Request) -> Response:
     random_pk = random.choice(pks)
     random_character = Characters.objects.get(pk=random_pk)
     serializer = CharacterSerializer(random_character)
-    return Response(serializer.data)
+    return Response(serializer.data, status=HTTP_200_OK)
+
+
+class ListCharacters(ListAPIView):
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self):
+        queryset = Characters.objects.all()
+
+        name = self.request.query_params.get("name")
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset
